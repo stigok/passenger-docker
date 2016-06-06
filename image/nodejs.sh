058196d87f81925754c477ed0b143f3fa481e413
stigok <stig@stigok.com>
2016-06-06 18:57:52 +0200

    Install Node.js with NVM (xtuple/nvm)

--- image/nodejs.sh
+++ image/nodejs.sh
@@ -3,5 +3,8 @@ set -e
 source /pd_build/buildconfig
 set -x
 
-## Install Node.js (also needed for Rails asset compilation)
-apt-get install -y nodejs
+# Install node with Node Version Manager (forked version xtuple/nvm)
+apt-get install -y wget
+wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | sudo bash
+nvm install v6.2.1
+nvm use v6.2.1
