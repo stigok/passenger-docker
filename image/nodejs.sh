e9e3b1b9c42787c3250706a8befd18165cb9fbb2
Stig Otnes Kolstad <stigok@gmail.com>
2016-11-04 01:25:50 +0100

    Properly install node and npm

--- image/nodejs.sh
+++ image/nodejs.sh
@@ -5,6 +5,8 @@ set -x
 
 # Install node with Node Version Manager (forked version xtuple/nvm)
 minimal_apt_get_install -y wget
-wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | sudo bash
+wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | bash
 nvm install v6.9.1
 nvm use v6.9.1
+ln -s /usr/local/nvm/v6.9.1/bin/npm /usr/bin/npm
+ln -s /usr/local/nvm/v6.9.1/bin/node /usr/bin/node
